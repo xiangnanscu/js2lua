3f918b45a628cdce821c817bd1cfa4b24155d438
xiangnan <280145668@qq.com>
2023-09-07 01:04:29 +0800

    feat:add-more-docs

--- README.template.md
+++ README.template.md
@@ -5,5 +5,51 @@ Writing LuaJIT with the expressiveness of JavaScript.
 ```sh
 npm install -g @xiangnanscu/js2lua
 ```
+# Usage
+## command
+concat one or more js files and transform them to one lua string
+```sh
+js2lua [options] file1, file2, ...
+```
+where options are:
+```js
+const defaultOptions = {
+  debug: false,
+  importStatementHoisting: true,
+  transformToString: true,
+  transformString: true,
+  transformJSONStringify: true,
+  transformJSONParse: true,
+  transformParseFloat: true,
+  transformParseInt: true,
+  transformNumber: true,
+  transformIsArray: true,
+  transformConsoleLog: true,
+  moduleExportsToReturn: true,
+  index0To1: true,
+  tryTranslateClass: true,
+  selfOperatorToCallback: true,
+  renameCatchErrorIfNeeded: true,
+  disableClassCall: true,
+};
+```
+### examples
+### basic:
+```sh
+js2lua foo.js > foo.lua
+```
+### to disable a feature (--no-[option_name]):
+```sh
+js2lua --no-transformToString foo.js
+```
+### to enable a feature:
+```sh
+js2lua --debug foo.js
+```
+## api
+```js
+import { js2lua } from 'js2lua';
+js2lua(`let a = 1`, {importStatementHoisting:true})
+```
 # Features
 [CODE_TABLE]
--- bin/js2lua.js
+++ bin/js2lua.js
@@ -1,22 +1,38 @@
 #!/usr/bin/env node
 
-import { js2lua } from '../src/js2lua.mjs';
-import fs from 'fs';
-import yargsParser from 'yargs-parser';
+import { js2lua } from "../src/js2lua.mjs";
+import fs from "fs";
+import yargsParser from "yargs-parser";
 
 const argv = yargsParser(process.argv.slice(2), {
-  boolean: ['option1', 'option2'],
+  boolean: [
+    "debug",
+    "importStatementHoisting",
+    "transformToString",
+    "transformString",
+    "transformJSONStringify",
+    "transformJSONParse",
+    "transformParseFloat",
+    "transformParseInt",
+    "transformNumber",
+    "transformIsArray",
+    "transformConsoleLog",
+    "moduleExportsToReturn",
+    "index0To1",
+    "tryTranslateClass",
+    "selfOperatorToCallback",
+    "renameCatchErrorIfNeeded",
+    "disableClassCall",
+  ],
   alias: {
-    o1: 'option1',
-    o2: 'option2',
+    // o1: "option1",
   },
 });
 
-
 const files = argv._;
-
-files.forEach(file => {
-  const jsCode = fs.readFileSync(file, 'utf8');
+// console.log(argv);
+files.forEach((file) => {
+  const jsCode = fs.readFileSync(file, "utf8");
   const luaCode = js2lua(jsCode, argv);
   console.log(luaCode);
 });

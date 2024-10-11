465a5437b05321223ee3e91cce1213df57a1e14b
xiangnan <280145668@qq.com>
2024-10-11 09:29:18 +0800

    test:cloudflare

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from "@vitejs/plugin-vue";
 // import requireT from 'vite-plugin-require-transform'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.CLOUDFLARE ? "/" : "/js2lua/",
+  // base: process.env.CLOUDFLARE ? "/" : "/js2lua/",
+  base:"/",
   plugins: [
     vue(),
     //   requireT({

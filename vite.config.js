66aa1a468dee200ffa0cdc18b21a692c6c4dd406
xiangnan <280145668@qq.com>
2024-10-11 10:09:02 +0800

    test:relative

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.CLOUDFLARE ? "/" : "/js2lua/",
-  base:"/",
+  base:"./",
   plugins: [
     vue(),
     //   requireT({
